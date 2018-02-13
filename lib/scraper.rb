a90868a7f622dd5304158ab1d21add9047846420
Alexander Urizar <urizaraj@gmail.com>
2018-02-13 17:38:52 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,8 +4,8 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
-  
-    def print_courses
+
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title
@@ -15,8 +15,5 @@ class Scraper
       end
     end
   end
-  
+
 end
-
-
-
